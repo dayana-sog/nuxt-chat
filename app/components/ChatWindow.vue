55607cb4e52d8870d2ff33057cebcf76e065d22a
dgoncalves <dgoncalves@pdmfc.com>
2025-09-15 21:57:31 +0100

    Feat - class: How to decide between pages, layouts and components

--- app/components/ChatWindow.vue
+++ app/components/ChatWindow.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
-const { chat, messages, sendMessage } = useChat()
-const { scrollToBottom, showScrollButton } = useChatScroll()
+import type { ChatMessage, Chat } from '../types'
+
+const props = defineProps<{
+  messages: ChatMessage[]
+  chat: Chat
+}>()
+
+const emit = defineEmits(['send-message'])
+
+const { showScrollButton, scrollToBottom, pinToBottom } = useChatScroll()
 
 function handleSendMessage(message: string) {
-  sendMessage(message)
+  emit('send-message', message)
 }
+
+watch(() => props.messages, pinToBottom, { deep: true })
 </script>
 
 <template>
